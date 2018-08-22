d444fc47c7bb8196a6972951f35b3c14c2e9a954
slava pocheptsov <pocheptsov@gmail.com>
2018-08-22 11:06:40 -0400

    refactor build script

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
 #!/bin/bash -x
 
-CIRCLE_CI_IMAGE_TAG=2.0.0
+CIRCLE_CI_IMAGE_TAG="${1:-2.0.0}"
 
 # https://github.com/cambridgebrainsciences/circleci
 
+# delete previous local tag
+if git tag --list | grep -qE "^${CIRCLE_CI_IMAGE_TAG}$"; then
+  git tag -d "$CIRCLE_CI_IMAGE_TAG"
+fi
+
 git tag -a "$CIRCLE_CI_IMAGE_TAG" -m "$CIRCLE_CI_IMAGE_TAG automatic deployment tag"
 
-git push origin "$CIRCLE_CI_IMAGE_TAG"
+git push origin --force "$CIRCLE_CI_IMAGE_TAG"
